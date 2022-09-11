5aacfb05e1c91cbd1dcfd38937075b7d9da4f3c1
Marina Sundström <marna.li@outlook.com>
2022-09-11 15:40:11 +0200

    User Interface

--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 [RobotApp](https://github.com/marinasundstrom/RobotApp) ported to TypeScript/JavaScript.
 
 Latest NodeJS required.
+
+To build:
+
+```sh
+npm build
+```
+
+To run the program:
+
+```sh
+npm start
+```
+
+To run tests:
+
+```sh
+npm test
+```
--- src/index.ts
+++ src/index.ts
@@ -4,28 +4,52 @@ import World from './world.js';
 import Point from './point.js';
 import Robot, { Instruction, Direction } from "./robot.js";
 
-/*
 const rl = readline.createInterface({ input, output });
 
-const answer = await rl.question('What do you think of Node.js? ');
+const worldSetup = await rl.question('World:');
+const worldParts = worldSetup.split(' ');
+const width = parseInt(worldParts[0]);
+const depth = parseInt(worldParts[1]);
 
-console.log(`Thank you for your valuable feedback: ${answer}`);
+const world = new World(width, depth);
+
+const robotSetup = await rl.question('Robot:');
+const robotParts = robotSetup.split(' ');
+const x = parseInt(robotParts[0]);
+const y = parseInt(robotParts[1]);
+const d = robotParts[2];
+
+let direction: Direction = undefined;
+switch(d) {
+    case "N": direction = Direction.north; break;
+    case "E": direction = Direction.east; break;
+    case "S": direction = Direction.south; break;
+    case "W": direction = Direction.west; break;
+}
+
+const robot = new Robot(world, new Point(x, y), direction);
+
+while(true) {
+    const instructionSetup = await rl.question('Command:');
+    const instructions = Array.from(instructionSetup)
+
+    for(let instruction of instructions) {
+        let instr: Instruction = undefined;
+        switch(instruction) {
+            case 'R': instr = Instruction.right; break;
+            case 'L': instr = Instruction.left; break;
+            case 'F': instr = Instruction.forward; break;
+        }
+
+        try {
+            robot.receiveCommand(instr);
+        } catch(error) {
+            console.log(error);
+            break;
+        }
+    }
+    
+    console.log(`Report: ${robot.position.x} ${robot.position.y} ${Direction[robot.direction][0].toUpperCase()}`);
+}
 
 rl.close();
-*/
-
-const world = new World(5, 5);
-const robot = new Robot(world, new Point(0, 0), Direction.east);
-
-let result = robot.receiveCommand(
-    Instruction.right,
-    Instruction.forward,
-    Instruction.left,
-    Instruction.forward,
-    Instruction.forward,
-    Instruction.left,
-    Instruction.right,
-    Instruction.forward
-);
-
-console.log(result);
